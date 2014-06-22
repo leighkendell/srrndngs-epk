20edcc3531ecd13fe0d84f015427f9d3372b1a49
Leigh Kendell <leighkendell@gmail.com>
2014-06-22 23:51:50 +0800

    Added waypoints for sticky nag

---
+++ js/site.js
@@ -0,0 +1,12 @@
+smoothScroll.init({
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 120, // Integer. How far to offset the scrolling anchor location in pixels
+    callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
+    callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
+});
+
+$('#sticky-trigger').waypoint(function() {
+  $('#main-nav').toggleClass('sticky');
+});
