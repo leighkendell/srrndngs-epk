3cc07582f922318f200db88277c5e96e3b6c07c6
Leigh Kendell <leigh@stormbox.com.au>
2014-06-23 11:17:44 +0800

    Optimised images + added social links

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ smoothScroll.init({
     speed: 500, // Integer. How fast to complete the scroll in milliseconds
     easing: 'easeInOutCubic', // Easing pattern to use
     updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-    offset: 120, // Integer. How far to offset the scrolling anchor location in pixels
+    offset: 93, // Integer. How far to offset the scrolling anchor location in pixels
     callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
     callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
 });
