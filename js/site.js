397c008a8e3aaceb47c0c2a202dca496a1bd889b
Leigh Kendell <leighkendell@gmail.com>
2014-06-28 15:53:33 +0800

    Added new content and removed js for current nav item

--- js/site.js
+++ js/site.js
@@ -10,28 +10,3 @@ smoothScroll.init({
 $('#sticky-trigger').waypoint(function() {
   $('#main-nav, body').toggleClass('sticky');
 });
-
-$('#bio').waypoint(function() {
-  $('.menu-item').removeClass('active');
-  $('.menu-item-bio').toggleClass('active');
-});
-
-$('#music').waypoint(function() {
-  $('.menu-item').removeClass('active');
-  $('.menu-item-music').toggleClass('active');
-});
-
-$('#videos').waypoint(function() {
-  $('.menu-item').removeClass('active');
-  $('.menu-item-videos').toggleClass('active');
-});
-
-$('#photos').waypoint(function() {
-  $('.menu-item').removeClass('active');
-  $('.menu-item-photos').toggleClass('active');
-});
-
-$('#contact').waypoint(function() {
-  $('.menu-item').removeClass('active');
-  $('.menu-item-contact').toggleClass('active');
-});
